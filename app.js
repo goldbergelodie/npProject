3aa3645c38fb935414ab69dd71aeee198441dba5
goldbergelodie <goldbergelodie@gmail.com>
2020-11-04 19:30:36 +0100

    Layout

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3000;
 
 // Static files
 app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/fonts', express.static(__dirname + 'public/fonts'))
 
 // Set templating engine
 app.use(expressLayouts);
---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+/* Hamburger menu */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  } 
+
